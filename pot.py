354ae9d0661844ec792befd7f989733f696a3b48
Adrian Akison <adrian@akison.com>
2026-09-19 16:43:07 +1000

    Try hardware management of chip select.

--- pot.py
+++ pot.py
@@ -1,27 +1,16 @@
 import spidev
 import time
-import RPi.GPIO as GPIO
-
-# Initialize GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7, GPIO.OUT)
-GPIO.output(7, GPIO.HIGH)  # Start with CS HIGH (inactive)
 
 # Initialize SPI
 spi = spidev.SpiDev()
-spi.open(0, 1)  # SPI bus 0, CE1 (GPIO7)
+spi.open(0, 1)  # SPI bus 0, CE1 (GPIO7) - CS handled by hardware
 spi.max_speed_hz = 10000
 spi.mode = 0
-spi.no_cs = True  # Disable automatic CS control
 
 def read_mcp3008(channel):
     if channel < 0 or channel > 7:
         raise ValueError("Channel must be 0-7")
-    # Manually toggle CS
-    GPIO.output(7, GPIO.LOW)  # Activate MCP3008
-    adc = spi.xfer2([1, (8 + channel) << 4, 0])  # SPI transfer
-    GPIO.output(7, GPIO.HIGH)  # Deactivate MCP3008
+    adc = spi.xfer2([1, (8 + channel) << 4, 0])  # SPI transfer, hardware CS
     data = ((adc[1] & 3) << 8) + adc[2]  # Combine 10-bit result
     return data
 
@@ -38,5 +27,4 @@ try:
 except KeyboardInterrupt:
     print("\nProgram terminated by user.")
 finally:
-    GPIO.cleanup()  # Restore GPIO state
     spi.close()  # Close SPI connection
